1cb4e7bb7a32f54bf9a7aa8dcdf7fcbeaa14393c
Riley <riley.palmer.gamedesign@gmail.com>
2020-04-29 11:32:51 +1000

    Street Scene, assets

---
+++ Camera Guy BI/Assets/Scripts/Ai/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    NavMeshAgent nav;
+    GameObject player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().gameObject;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 8)
+            nav.destination = player.transform.position;
+    }
+}
---
+++ Camera Guy BI/Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+public Transform spawnPoint;
+
+ public void OnTriggerEnter(Collider other)
+  {
+        // detects if the player interacts 
+	  if (other.transform.CompareTag("Player"))
+	  {
+            // teleports the player to the location
+		  other.transform.position = spawnPoint.position;
+	  }
+  }
+}
