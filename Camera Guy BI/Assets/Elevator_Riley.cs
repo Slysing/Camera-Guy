83cb2d960107178af7931c9b22494dd1424018d0
Riley <riley.palmer.gamedesign@gmail.com>
2020-05-11 10:07:50 +1000

    Main Scene added, Created Elevator, Scene Changes

---
+++ Camera Guy BI/Assets/Elevator_Riley.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator_Riley : MonoBehaviour
+{
+    public Transform elevatorTransform;
+    public float minHeight;
+    public float maxHeight;
+
+    bool hasPlayer;
+    bool moving;
+    bool movingUp;
+
+   public float movementSpeed = 5;
+
+    void Start()
+    {
+        if (elevatorTransform == null)
+            elevatorTransform = transform;
+    }
+
+
+    void Update()
+    {
+
+        // Temp - to be replaced with dialogue system
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            UseElevator();
+        }
+
+
+
+        if (moving)
+        {
+            float newHeight = elevatorTransform.position.y;
+
+            if (movingUp)
+            {
+                newHeight += movementSpeed * Time.deltaTime;
+                if (newHeight > maxHeight)
+                    moving = false;
+            }
+            else
+            {
+                newHeight -= movementSpeed * Time.deltaTime;
+                if (newHeight < minHeight)
+                    moving = false;
+            }
+
+            newHeight = Mathf.Clamp(newHeight, minHeight, maxHeight);
+
+            elevatorTransform.position = new Vector3(elevatorTransform.position.x, newHeight, elevatorTransform.position.z);
+
+        }
+    }
+
+
+    public void UseElevator()
+    {
+        if (hasPlayer && !moving)
+        {
+            ToggleDirection();
+            moving = true;
+        }
+    }
+
+
+    void ToggleDirection()
+    {
+        movingUp = !movingUp;
+    }
+
+    void OnTriggerEnter (Collider collider)
+    {
+        if (collider.transform.CompareTag("Player"))
+        {
+            hasPlayer = true;
+            collider.transform.SetParent(transform);
+        }
+       Debug.Log("Enter");
+    }  
+    void OnTriggerExit (Collider collider)
+    {
+        if (collider.transform.CompareTag("Player"))
+        {
+            hasPlayer = false;
+            collider.transform.SetParent(null);
+        }
+            Debug.Log("Exit");
+    }  
+
+}
