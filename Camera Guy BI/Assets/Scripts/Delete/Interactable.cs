6893912525383adab5fe5e473039252a16a33ee9
Riley <riley.palmer.gamedesign@gmail.com>
2020-05-12 23:57:52 +1000

    Main scene changes, temp assets like Gangster and Skybox, WIP scripts

---
+++ Camera Guy BI/Assets/Scripts/Delete/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset; 
+    public string message; 
+    public bool canTurn;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();   
+        meshFilter = GetComponent<MeshFilter>();           
+           }
+}
