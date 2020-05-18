c60f145110997c20b48508efc59da2b59dbeda4d
Slysing <48895316+Slysing@users.noreply.github.com>
2020-05-19 03:43:11 +1000

    Start Game and Exit Game Scripts

---
+++ Camera Guy BI/Assets/FinnsChanges/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+
+    public void quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+}
